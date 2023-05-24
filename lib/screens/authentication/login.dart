8cd150b9720318f0180268bf9a365fb70d913ed1
Guilherme Garcia <gui123.garcia@gmail.com>
2023-05-24 19:09:45 -0300

    starting project

---
+++ lib/screens/authentication/login.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:streamingapp/utils/constants.dart';
+import 'package:streamingapp/shared/rounded_button.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/screens/authentication/signup.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  const SignUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
